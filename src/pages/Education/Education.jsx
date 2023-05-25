5e44b3438bc7f1539e009c1da641b7cd69d98f78
Tanishq <tanishqjain1652003@gmail.com>
2023-05-25 15:28:52 +0530

    education page less responsive

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,9 +1,23 @@
+import EducationTimeline from "../../components/EducationTimeline/EducationTimeline";
 import "./Education.css";
+import educationImg from "../../assets/educationImg.png";
 
 function Education() {
   return (
-    <div>
-        This is the Education page
+    <div className="education">
+      <div className="educationLanding">
+        <div className="educationLandingLeft">
+          <h1>Educational <span>Background</span></h1>
+          <p>
+          An investment in knowledge pays the best interest. <br /> <span>– Benjamin Franklin</span>
+          </p>
+        </div>
+        <div className="educationLandingRight">
+          <img src={educationImg} alt="" />
+        </div>
+      </div>
+      <h1 className="educationHeading">FORMAL <span>EDUCATION</span></h1>
+      <EducationTimeline />
     </div>
   )
 }
