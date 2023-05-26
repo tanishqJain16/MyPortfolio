0968daff9abff8f8326e858753f36e2439acc0ff
Tanishq <tanishqjain1652003@gmail.com>
2023-05-26 16:05:22 +0530

    project page completed

---
+++ src/components/SmallCard/SmallCard.jsx
@@ -0,0 +1,29 @@
+import "./SmallCard.css"
+import GithubIcon from '@material-ui/icons/GitHub';
+import LinkIcon from '@material-ui/icons/Link';
+
+
+function SmallCard(props) {
+    const handleGithub = () => {                {/* eslint-disable-next-line react/prop-types */}
+        window.open(props.github, "_blank");
+    }
+
+    const handleLink = () => {                  {/* eslint-disable-next-line react/prop-types */}
+        window.open(props.link, "_blank");
+    }
+
+    return (
+        <div className="smallCard">             {/* eslint-disable-next-line react/prop-types */}
+            <img src={props.img} alt="" />
+            <div className="smallCardP">        {/* eslint-disable-next-line react/prop-types */}
+                <p>{props.title}</p>
+                <div className="smallCard__icon">
+                    <GithubIcon onClick={handleGithub} />
+                    <LinkIcon onClick={handleLink}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SmallCard
--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -1,9 +1,24 @@
 import "./Project.css";
+import jsrimg from "../../assets/jsr-img.png";
+import myportfolioimg from "../../assets/myportfolio-img.png";
+import sphurtiimg from "../../assets/sphurti-img.png";
+import SmallCard from "../../components/SmallCard/SmallCard";
+import Particlejs from "../../components/ParticleJs/Particlejs";
 
 function Project() {
   return (
-    <div>
-        This is the Project page
+    <div className="project">
+      <Particlejs/>
+      <div className="project__container">
+        <h1 className="project__container__title">
+          My <span>Projects</span>
+        </h1>
+        <div className="project__container__card">
+          <SmallCard img={jsrimg} title={"JSR Forms"} github={"https://github.com/tanishqJain16/form-website"} link={"https://www.jsrforms.live/"} />
+          <SmallCard img={sphurtiimg} title={"Sphurti'23"} github={"https://github.com/tanishqJain16/sphurtiWebsite2.0"} link={"https://ditusphurti.com/"} />
+          <SmallCard img={myportfolioimg} title={"My Portfolio"} github={"https://github.com/tanishqJain16/MyPortfolio"} link={"https://my-portfolio-tanishqjain16.vercel.app/"} />
+        </div>
+      </div>
     </div>
   )
 }
