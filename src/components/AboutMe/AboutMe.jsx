6ecdfe9399ce8b902bafe332a56bf0180d5f523f
Tanishq <tanishqjain1652003@gmail.com>
2023-05-30 23:51:51 +0530

    fix issues and added animation on home

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,26 +1,36 @@
 import "./AboutMe.css"
 import aboutImg from "../../assets/aboutMe.jpeg"
+import { useNavigate } from "react-router-dom"
+import { Fade } from "react-awesome-reveal";
 
 function AboutMe() {
+    const navigate = useNavigate();
+    const handleContactMe = () => {
+        navigate("/contact");
+    }
     return (
         <div className="aboutMe">
             <div className="aboutMeLeft">
-                <div className="aboutMeLeft__img">
-                    <img src={aboutImg} alt="aboutme-img" />
-                </div>
+                <Fade className="fade" duration={2000} triggerOnce={true} delay={500}>
+                    <div className="aboutMeLeft__img">
+                        <img src={aboutImg} alt="aboutme-img" />
+                    </div>
+                </Fade>
             </div>
             <div className="aboutMeRight">
-                <div className="aboutHeading">
-                    <h1><span>About</span> Me</h1>
-                </div>
-                <div className="aboutMeRight__content">
-                    <p>
-                        I am a 3rd year student pursuing B.Tech in Computer Science and Engineering from DIT University, Dehradun. I am a <span>Full Stack Developer</span> with good knowledge of <span>Frontend</span> and <span>Backend</span> techniques. I love spending time on <span>fixing little details</span> and <span>optimizing</span> web apps. Also I like working in a <span>team</span> and <span>collaborating</span> with people.
-                    </p>
-                    <button className="contactMeBtn">
-                        Contact Me
-                    </button>
-                </div>
+                <Fade className="fade" duration={2000} triggerOnce={true} delay={1000}>
+                    <div className="aboutHeading">
+                        <h1><span>About</span> Me</h1>
+                    </div>
+                    <div className="aboutMeRight__content">
+                        <p>
+                            I am a 3rd year student pursuing B.Tech in Computer Science and Engineering from DIT University, Dehradun. I am a <span>Full Stack Developer</span> with good knowledge of <span>Frontend</span> and <span>Backend</span> techniques. I love spending time on <span>fixing little details</span> and <span>optimizing</span> web apps. Also I like working in a <span>team</span> and <span>collaborating</span> with people.
+                        </p>
+                        <button className="contactMeBtn" onClick={handleContactMe}>
+                            Contact Me
+                        </button>
+                    </div>
+                </Fade>
             </div>
         </div>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,18 @@ import Particlejs from "../../components/ParticleJs/Particlejs";
 import Socials from "../../components/Socials/Socials";
 // import { Link } from "react-router-dom";
 import AboutMe from "../../components/AboutMe/AboutMe";
+import { Fade } from "react-awesome-reveal";
 
 function Home() {
     const handleAboutMe = () => {
-        window.scrollTo({ left: 0, top: 800, behavior: "smooth" });
+        window.scrollTo({ left: 0, top: 790, behavior: "smooth" });
     }
     return (
         <div className="home">
             <Particlejs />
             <div className="homeLanding">
                 <div className="home-left">
-                    <h1>{`Hi, I'm`}  <TypeAnimation
+                    <Fade cascade className="fade" duration={2000} triggerOnce={true}><h1>{`Hi, I'm`}  <TypeAnimation
                         className="home-left__name--color"
                         sequence={[1000,
                             'Tanishq',
@@ -25,37 +26,41 @@ function Home() {
                         repeat={0}
                         style={{ display: 'inline-block' }}
                     /></h1>
-                    <h2 className="home-left__subheading"><TypeAnimation
-                        className="home-left__subheading--color"
-                        sequence={[
-                            1000,
-                            `I'm a Web Developer`,
-                        ]}
-                        wrapper="span"
-                        cursor={true}
-                        repeat={Infinity}
-                        style={{ display: 'inline-block' }}
-                    /></h2>
-                    <div className="socials">
-                        <Socials />
-                    </div>
-                    <div className="homeButtons">
-                        <button className="cvbtn">
-                            <a href="https://drive.google.com/file/d/1ESrN_tBnAOOCCQJ2PVkuU1p1SuQd1N91/view?usp=share_link" target="_blank" rel="noreferrer">
-                                View CV
-                            </a>
-                        </button>
-                        <button className="aboutMeBtn" onClick={handleAboutMe}>About Me
-                        </button>
-                    </div>
+                        <h2 className="home-left__subheading"><TypeAnimation
+                            className="home-left__subheading--color"
+                            sequence={[
+                                1000,
+                                `I'm a Web Developer`,
+                            ]}
+                            wrapper="span"
+                            cursor={true}
+                            repeat={Infinity}
+                            style={{ display: 'inline-block' }}
+                        /></h2>
+                        <div className="socials">
+                            <Socials />
+                        </div>
+                        <div className="homeButtons">
+                            <button className="cvbtn">
+                                <a href="https://drive.google.com/file/d/1ESrN_tBnAOOCCQJ2PVkuU1p1SuQd1N91/view?usp=share_link" target="_blank" rel="noreferrer">
+                                    View CV
+                                </a>
+                            </button>
+                            <button className="aboutMeBtn" onClick={handleAboutMe}>About Me
+                            </button>
+                        </div>
+                    </Fade>
                 </div>
                 <div className="home-right">
-                    <img src={landingimg} alt="landing-img" className="home-right__img" />
+                    <Fade className="fade" duration={5000} delay={1000} triggerOnce={true}>
+                        <img src={landingimg} alt="landing-img" className="home-right__img" />
+                    </Fade>
                 </div>
             </div>
             <div className="homeAbout">
                 <AboutMe />
             </div>
+
         </div>
     )
 }
